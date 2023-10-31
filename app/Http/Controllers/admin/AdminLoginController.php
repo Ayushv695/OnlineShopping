31f87ff4d8e98d43b90ab16aecb2b544ed0d67d6
Ayush <vermaayush695@gmail.com>
2023-11-01 00:24:43 +0530

    log

---
+++ app/Http/Controllers/admin/AdminLoginController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminLoginController extends Controller
+{
+    public function index(){
+        return view('admin.login');
+    }
+
+    public function authenticate(Request $request){
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return redirect()->route('admin.login')->withErrors($validator)->withInput($request->only('email'));
+        }
+        else{
+            if(Auth::guard('admin')->attempt(['email'=>$request->email,'password'=>$request->password],$request->get('remember'))){
+                $admin = Auth::guard('admin')->user();
+                if($admin->role == "admin"){
+                    return redirect()->route('admin.dashboard');
+                }
+                else{
+                    Auth::guard('admin')->logout();
+                    return redirect()->route('admin.login')->with("error","You are not authorized to access admin panel");
+                }
+            }
+            else{
+                return redirect()->route('admin.login')->with("error","Either Email/Password is incorrect");
+            }
+        }
+    }
+}
